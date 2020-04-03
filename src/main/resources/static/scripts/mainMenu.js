1d46f46e2f2e6d80356676ab63f553340cbeff85
atkalmes <60158525+atkalmes@users.noreply.github.com>
2020-04-02 21:27:37 -0500

    Update mainMenu.js

--- src/main/resources/static/scripts/mainMenu.js
+++ src/main/resources/static/scripts/mainMenu.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	getStartTransactionActionElement().addEventListener(
 		"click",
-		() => { displayError("Functionality has not yet been implemented."); });
+		() => { window.location.assign("/productDetail"); });
 
 	getViewProductsActionElement().addEventListener(
 		"click",
