6a75b7e5d084c1cd55004ef7b8427689107916d7
atkalmes <60158525+atkalmes@users.noreply.github.com>
2020-04-14 00:33:48 -0500

    Create ProductByPartialLookupCodeQuery.java

---
+++ src/main/java/edu/uark/registerapp/commands/products/ProductByPartialLookupCodeQuery.java
@@ -0,0 +1,53 @@
+package edu.uark.registerapp.commands.products;
+
+import java.util.Optional;
+
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.uark.registerapp.commands.ResultCommandInterface;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
+import edu.uark.registerapp.commands.exceptions.UnprocessableEntityException;
+import edu.uark.registerapp.models.api.Product;
+import edu.uark.registerapp.models.entities.ProductEntity;
+import edu.uark.registerapp.models.repositories.ProductRepository;
+
+@Service
+public class ProductByPartialLookupCodeQuery implements ResultCommandInterface<Product[]> {
+	@Override
+	public Product[] execute() {
+		this.validateProperties();
+		
+    List<Product> products = new LinkedList<Product>();
+		final List<ProductEntity> productEntities =
+			this.productRepository.findByLookupCodeContainingIgnoreCase(
+      this.partialLookupCode);
+      
+      for (ProductEntity productEntity : productEntities){
+        
+        
+      }
+	}
+
+	// Helper methods
+	private void validateProperties() {
+		if (StringUtils.isBlank(this.partialLookupCode)) {
+			throw new UnprocessableEntityException("partialLookupCode");
+		}
+	}
+
+	// Properties
+	private String partialLookupCode;
+	public String getPartialLookupCode() {
+		return this.partialLookupCode;
+	}
+	public ProductByPartialLookupCodeQuery setPartialLookupCode(final String partialLookupCode) {
+		this.partialLookupCode = partialLookupCode;
+		return this;
+	}
+
+	@Autowired
+	private ProductRepository productRepository;
+}
