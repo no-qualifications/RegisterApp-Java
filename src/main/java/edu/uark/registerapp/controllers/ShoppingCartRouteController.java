85ed9521b717f6d575e48c8ff33fa2d7e0d3f37d
chrissouvan <47071652+chrissouvan@users.noreply.github.com>
2020-04-29 10:52:19 -0500

    Update ShoppingCartRouteController.java

--- src/main/java/edu/uark/registerapp/controllers/ShoppingCartRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/ShoppingCartRouteController.java
@@ -50,13 +50,13 @@ public class ShoppingCartRouteController extends BaseRouteController {
 			ViewModelNames.PRODUCT.getValue(),
 			(new Product()).setLookupCode(StringUtils.EMPTY).setCount(0));
 
-		/*
+		
 		try {
 			this.productByLookupCodeQuery.execute();
 		} catch (NotFoundException e) {
-			return new ModelAndView(REDIRECT_PREPEND.concat(ViewNames.EMPLOYEE_DETAIL.getRoute()));
+			modelAndView.addObject(ViewNames.SHOPPING_CART.getViewName(), queryParameters);
 		}
-		*/
+		
 
 		return modelAndView;
 	}
