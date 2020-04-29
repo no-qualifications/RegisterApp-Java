6958329fea4ed3c6d1b989f49ec91be158e139ea
chrissouvan <47071652+chrissouvan@users.noreply.github.com>
2020-04-29 13:27:49 -0500

    Update ShoppingCartRouteController.java

--- src/main/java/edu/uark/registerapp/controllers/ShoppingCartRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/ShoppingCartRouteController.java
@@ -18,6 +18,7 @@ import org.springframework.web.servlet.ModelAndView;
 import edu.uark.registerapp.commands.exceptions.NotFoundException;
 import edu.uark.registerapp.commands.products.ProductByLookupCodeQuery;
 import edu.uark.registerapp.commands.products.ProductQuery;
+import edu.uark.registerapp.commands.products.ProductsQuery;
 import edu.uark.registerapp.controllers.enums.ViewModelNames;
 import edu.uark.registerapp.controllers.enums.ViewNames;
 import edu.uark.registerapp.models.api.Product;
@@ -57,6 +58,18 @@ public class ShoppingCartRouteController extends BaseRouteController {
 			modelAndView.addObject(ViewNames.SHOPPING_CART.getViewName(), queryParameters);
 		}
 		*/
+		try {
+			modelAndView.addObject(
+				ViewModelNames.PRODUCTS.getValue(),
+				this.productsQuery.execute());
+		} catch (final Exception e) {
+			modelAndView.addObject(
+				ViewModelNames.ERROR_MESSAGE.getValue(),
+				e.getMessage());
+			modelAndView.addObject(
+				ViewModelNames.PRODUCTS.getValue(),
+				(new Product[0]));
+		}
 
 		return modelAndView;
 	}
@@ -108,4 +121,7 @@ public class ShoppingCartRouteController extends BaseRouteController {
 
 	@Autowired
 	private ProductByLookupCodeQuery productByLookupCodeQuery;
+
+	@Autowired
+	private ProductsQuery productsQuery;
 }
