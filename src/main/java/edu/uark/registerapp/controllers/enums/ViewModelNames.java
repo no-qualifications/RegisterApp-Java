4229806c268ddeab58cfb6e2861a2e175ff17e99
chrissouvan <47071652+chrissouvan@users.noreply.github.com>
2020-04-29 15:40:02 -0500

    Update ViewModelNames.java

--- src/main/java/edu/uark/registerapp/controllers/enums/ViewModelNames.java
+++ src/main/java/edu/uark/registerapp/controllers/enums/ViewModelNames.java
@@ -8,8 +8,8 @@ public enum ViewModelNames {
 	PRODUCT("product"), // Product detail
 	EMPLOYEE_ID("employeeId"), // Sign in
 	EMPLOYEE("employee"), // Employee detail
-	EMPLOYEE_TYPES("employeeTypes");
-	
+	EMPLOYEE_TYPES("employeeTypes"),
+	TRANSACTION_ENTRY("transactionEntry");
 	public String getValue() {
 		return value;
 	}
