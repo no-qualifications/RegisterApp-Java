ced34f1fece83f4c4519d37c577d009cd72dab6f
chrissouvan <47071652+chrissouvan@users.noreply.github.com>
2020-04-30 04:02:07 -0500

    2 new files

---
+++ src/main/java/edu/uark/registerapp/controllers/addToCartRouteController.java
@@ -0,0 +1,54 @@
+package edu.uark.registerapp.controllers;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import org.apache.commons.lang3.StringUtils;
+import javax.servlet.http.HttpServletRequest;
+import edu.uark.registerapp.controllers.enums.ViewModelNames;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.stereotype.Controller;
+import edu.uark.registerapp.models.api.Product;
+import org.springframework.web.bind.annotation.RequestMapping;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
+import edu.uark.registerapp.controllers.enums.ViewNames;
+
+@Controller
+@RequestMapping(value="/shoppingCartDetail.html")
+public class addToCartRouteController extends BaseRouteController {
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView start(
+		@RequestParam final Map<String, String> queryParameters,
+		final HttpServletRequest request
+	) {
+        final Optional<ActiveUserEntity> activeUserEntity =
+			this.getCurrentUser(request);
+		if (!activeUserEntity.isPresent()) {
+			return this.buildInvalidSessionResponse();
+		} else if (!this.isElevatedUser(activeUserEntity.get())) {
+			return this.buildNoPermissionsResponse(
+				ViewNames.PRODUCT_LISTING.getRoute());
+        }
+
+        
+        
+        final ModelAndView modelAndView =
+			this.setErrorMessageFromQueryString(
+				new ModelAndView(ViewNames.PRODUCT_DETAIL.getViewName()),
+                queryParameters);
+
+                modelAndView.addObject(
+                    ViewModelNames.IS_ELEVATED_USER.getValue(),
+                    true);
+                modelAndView.addObject(
+                    ViewModelNames.PRODUCT.getValue(),
+                    (new Product()).setLookupCode(StringUtils.EMPTY).setCount(0));
+        
+        return modelAndView;
+    }
+
+
+    
+}
