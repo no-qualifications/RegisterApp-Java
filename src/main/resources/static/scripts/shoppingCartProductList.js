716d1b1b26697ea81ae268cf4e00fadf45b0194f
chrissouvan <47071652+chrissouvan@users.noreply.github.com>
2020-04-30 03:28:23 -0500

    added shoppingCartProductList.js

---
+++ src/main/resources/static/scripts/shoppingCartProductList.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const productListElements = document.getElementById("productsListing").children;
+
+	for (let i = 0; i < productListElements.length; i++) {
+		productListElements[i].addEventListener("click", productClick);
+	}
+});
+
+function findClickedListItemElement(clickedTarget) {
+	if (clickedTarget.tagName.toLowerCase() === "li") {
+		return clickedTarget;
+	} else {
+		let ancestorIsListItem = false;
+		let ancestorElement = clickedTarget.parentElement;
+
+		while (!ancestorIsListItem && (ancestorElement != null)) {
+			ancestorIsListItem = (ancestorElement.tagName.toLowerCase() === "li");
+
+			if (!ancestorIsListItem) {
+				ancestorElement = ancestorElement.parentElement;
+			}
+		}
+
+		return (ancestorIsListItem ? ancestorElement : null);
+	}
+}
+
+function productClick(event) {
+	let listItem = findClickedListItemElement(event.target);
+
+	window.location.assign(
+		"/productDetail/"
+		+ listItem.querySelector("input[name='productId'][type='hidden']").value);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	const transactionListElements = document.getElementById("transactionListing").children;
+  
+	for(let i=0; i< transactionListElements.length; i++) {
+	  transactionListElements[i].addEventListener("click", transactionClick);
+	}
+  });
