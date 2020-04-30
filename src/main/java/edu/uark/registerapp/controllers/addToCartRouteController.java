c6c62e357873fc206c19119d97699b2beaa63b46
chrissouvan <47071652+chrissouvan@users.noreply.github.com>
2020-04-30 11:18:41 -0500

    Attempting addToCart routing

--- src/main/java/edu/uark/registerapp/controllers/addToCartRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/addToCartRouteController.java
@@ -2,18 +2,25 @@ package edu.uark.registerapp.controllers;
 
 import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.apache.commons.lang3.StringUtils;
 import javax.servlet.http.HttpServletRequest;
+
+import edu.uark.registerapp.commands.products.ProductQuery;
 import edu.uark.registerapp.controllers.enums.ViewModelNames;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import edu.uark.registerapp.models.api.Product;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import edu.uark.registerapp.models.entities.ActiveUserEntity;
 import edu.uark.registerapp.controllers.enums.ViewNames;
+import edu.uark.registerapp.models.enums.EmployeeClassification;
 
 @Controller
 @RequestMapping(value="/shoppingCartDetail")
@@ -48,6 +55,54 @@ public class addToCartRouteController extends BaseRouteController {
         
         return modelAndView;
     }
+
+
+    @RequestMapping(value="/{productId}", method=RequestMethod.GET)
+    public ModelAndView startWithProduct(
+        @PathVariable final UUID productId,
+        @RequestParam final Map<String, String> queryParameters,
+        final HttpServletRequest request
+    ) {
+
+        final Optional<ActiveUserEntity> activeUserEntity =
+			this.getCurrentUser(request);
+		if (!activeUserEntity.isPresent()) {
+			return this.buildInvalidSessionResponse();
+		}
+
+        final ModelAndView modelAndView =
+			this.setErrorMessageFromQueryString(
+				new ModelAndView(ViewNames.PRODUCT_DETAIL.getViewName()),
+                queryParameters);
+            
+        modelAndView.addObject(
+            ViewModelNames.IS_ELEVATED_USER.getValue(),
+                EmployeeClassification.isElevatedUser(
+                    activeUserEntity.get().getClassification()));
+
+                try {
+                    modelAndView.addObject(
+                        ViewModelNames.PRODUCT.getValue(),
+                        this.productQuery.setProductId(productId).execute());
+                } catch (final Exception e) {
+                    modelAndView.addObject(
+                        ViewModelNames.ERROR_MESSAGE.getValue(),
+                        e.getMessage());
+                    modelAndView.addObject(
+                        ViewModelNames.PRODUCT.getValue(),
+                        (new Product())
+                            .setCount(0)
+                            .setLookupCode(StringUtils.EMPTY));
+                }
+        
+                return modelAndView;
+
+    }
+
+
+    //Properties
+    @Autowired
+    private ProductQuery productQuery;
 
 
     
